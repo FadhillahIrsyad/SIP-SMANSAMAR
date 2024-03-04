9651f16ff8a4db6ed811ead351af27dbfea75676
Farrel <postfarrel@gmail.com>
2024-03-04 17:39:01 +0700

    feat: Create 1st stage database structure

---
+++ database/migrations/2024_03_04_103040_add_role_and_footprint_to_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('id_creator')->nullable();
+            $table->foreign('id_creator')->references( 'id' )->on('users');
+            $table->unsignedBigInteger('id_updater')->nullable();
+            $table->foreign( 'id_updater' )->references('id') ->on('users');
+            $table->unsignedBigInteger('id_role');
+            $table->foreign('id_role')->references( 'id' )->on('role_akun');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
