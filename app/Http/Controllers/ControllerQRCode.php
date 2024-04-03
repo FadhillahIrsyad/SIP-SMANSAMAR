63b9f354b30e9a7cef0c6cb462c23c1d01119d0a
Farrel <postfarrel@gmail.com>
2024-04-03 20:49:09 +0700

    feat: making qr generator function

--- app/Http/Controllers/ControllerQRCode.php
+++ app/Http/Controllers/ControllerQRCode.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Siswa;
 use Illuminate\Http\Request;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class ControllerQRCode extends Controller
 {
     //
     public function getData(){
-        return view('qrcode');
+        $data = Siswa::find('15'); // this will pull data from database
+        $qrCode = QrCode::generate(
+            $data
+        ); // this will generate qr code
+        return $qrCode; // this will return the qrcode that have been generated
     }
 }
