de5cba60e0463f02d880d7101168933ede4309ac
Farrel <postfarrel@gmail.com>
2024-05-09 23:00:47 +0700

    feat: add_func_import_csv

--- app/Http/Controllers/ControllerSiswa.php
+++ app/Http/Controllers/ControllerSiswa.php
@@ -34,4 +34,48 @@ class ControllerSiswa extends Controller
         $siswa->delete();
         return redirect()->route('Daftar Siswa');
     }
+
+    public function importCSV(Request $request){
+        $request->validate([
+            'import_csv' => 'required|mimes:csv',
+        ]);
+
+        $file = $request->file('import_csv');
+        $handle = fopen($file->path(),'r');
+
+        fgetcsv($handle);
+
+        $chunksize = 25;
+        while(!feof($handle)){
+            $chunkdata = [];
+
+            for($i=0; $i<$chunksize; $i++){
+                $data = fgetcsv($handle);
+                if($data === false){
+                    break;
+                }
+                $chunkdata[] = $data;
+            }
+            $this->getchunkdata($chunkdata);
+        }
+        fclose($handle);
+        
+        return redirect()->route('Daftar Siswa');
+    }
+
+    public function getchunkdata($chunkdata){
+        foreach($chunkdata as $column){
+            $nisn = $column[0];
+            $nama = $column[1];
+            $kelas = $column[2];
+            $no_hp_ortu = $column[3];
+
+            $siswa = new Siswa();
+            $siswa->nisn = $nisn;
+            $siswa->nama = $nama;
+            $siswa->kelas = $kelas;
+            $siswa->no_hp_ortu = $no_hp_ortu;
+            $siswa->save();
+        }
+    }
 }
